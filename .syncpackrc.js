1ea16a5bc0cd9e5090949df8623b52e2cbacd8eb
Apolo Freitas <apolobrener11@gmail.com>
2024-08-04 18:08:02 -0400

    ⬆️ Upgrade packages and migrate to eslint 9

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -1,8 +1,13 @@
 /** @type {import("syncpack").RcFile} */
 const config = {
   dependencyTypes: ['dev', 'peer', 'prod'],
-  semverRange: '',
   source: ['package.json', 'packages/*/package.json'],
+  semverGroups: [
+    {
+      range: '',
+      dependencies: ['**'],
+    },
+  ],
   versionGroups: [
     {
       label: 'Internal config packages should be pinned to "*" (meaning any version is acceptable)',
